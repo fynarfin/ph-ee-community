694a27b4665a04fac5b5734d2b18225fc392cb9f
Ankita Ranjan <ankita@fynarfin.io>
2024-02-02 17:47:37 +0530

    [GOV-83] P2G NonRTP TCs

--- ph-ee-connector-crm/src/main/java/org/mifos/connector/crm/routes/BillInquiryRouteBuilder.java
+++ ph-ee-connector-crm/src/main/java/org/mifos/connector/crm/routes/BillInquiryRouteBuilder.java
@@ -1,7 +1,6 @@
 package org.mifos.connector.crm.routes;
 
-import static org.mifos.connector.crm.utils.BillPayEnum.SUCCESS_RESPONSE_CODE;
-import static org.mifos.connector.crm.utils.BillPayEnum.SUCCESS_RESPONSE_MESSAGE;
+import static org.mifos.connector.crm.utils.BillPayEnum.*;
 import static org.mifos.connector.crm.zeebe.ZeebeVariables.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -12,10 +11,17 @@ import org.mifos.connector.crm.data.BillInquiryResponseDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BillInquiryRouteBuilder extends ErrorHandlerRouteBuilder {
+
+    @Value("${billPay.billIdInvalidId}")
+    private String billIdInvalidId;
+
+    @Value("${billPay.billIdEmptyId}")
+    private String billIdEmptyId;
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -30,17 +36,35 @@ public class BillInquiryRouteBuilder extends ErrorHandlerRouteBuilder {
 
         from("direct:bill-inquiry").routeId("bill-inquiry").log("Received request for bill inquiry")
                 .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(200)).process(exchange -> {
+                    logger.debug("Bill Inquiry Id: {}" , exchange.getIn().getHeader(BILL_ID).toString());
+                    BillInquiryResponseDTO billInquiryResponseDTO;
                     String billId = exchange.getIn().getHeader(BILL_ID).toString();
                     String billerID = exchange.getProperty(BILLER_ID).toString();
                     String billerName = exchange.getProperty(BILLER_NAME).toString();
-                    BillInquiryResponseDTO billInquiryResponseDTO = setResponseBody(
-                            exchange.getIn().getHeader(CLIENTCORRELATIONID).toString(), billId, billerID, billerName);
+                    if(billId.equals(billIdInvalidId)){
+                        logger.info("Bill Id is Invalid");
+                        billInquiryResponseDTO = setResponseBodyForInvalidBill(
+                                exchange.getIn().getHeader(CLIENTCORRELATIONID).toString());
+                        exchange.setProperty(BILL_FETCH_FAILED, true);
+                        exchange.setProperty(ERROR_INFORMATION,"Bill Fetch failed: Invalid Bill Id");
+                    }
+                    else if(billId.equals(billIdEmptyId)){
+                        logger.info("Bill Id is Empty");
+                        billInquiryResponseDTO = setResponseBodyForEmptyBill(
+                                exchange.getIn().getHeader(CLIENTCORRELATIONID).toString());
+                        exchange.setProperty(BILL_FETCH_FAILED, true);
+                        exchange.setProperty(ERROR_INFORMATION,"Bill Fetch failed: Empty bill ID");
+                    }
+                    else {
+                        billInquiryResponseDTO = setResponseBody(
+                                exchange.getIn().getHeader(CLIENTCORRELATIONID).toString(), billId, billerID, billerName);
+                        exchange.setProperty(BILL_FETCH_FAILED, false);
+                    }
                     exchange.setProperty(BILL_INQUIRY_RESPONSE, billInquiryResponseDTO);
-                    exchange.setProperty(BILL_FETCH_FAILED, false);
                     ObjectMapper objectMapper = new ObjectMapper();
                     String jsonString = objectMapper.writeValueAsString(billInquiryResponseDTO);
                     exchange.getIn().setBody(jsonString);
-                    logger.debug("Bill Inquiry Response: " + jsonString);
+                    logger.debug("Bill Inquiry Response: {}", jsonString);
                 });
     }
 
@@ -59,5 +83,18 @@ public class BillInquiryRouteBuilder extends ErrorHandlerRouteBuilder {
         billInquiryResponseDTO.setClientCorrelationId(clientCorrelationId);
         return billInquiryResponseDTO;
     }
+    private BillInquiryResponseDTO setResponseBodyForInvalidBill(String clientCorrelationId) {
+        billInquiryResponseDTO.setCode(FAILED_RESPONSE_CODE.getValue());
+        billInquiryResponseDTO.setReason("Invalid Bill ID");
+        billInquiryResponseDTO.setClientCorrelationId(clientCorrelationId);
+        return billInquiryResponseDTO;
+    }
+    private BillInquiryResponseDTO setResponseBodyForEmptyBill(String clientCorrelationId) {
+        billInquiryResponseDTO.setCode(FAILED_RESPONSE_CODE.getValue());
+        billInquiryResponseDTO.setReason("Empty Bill ID");
+        billInquiryResponseDTO.setClientCorrelationId(clientCorrelationId);
+        return billInquiryResponseDTO;
+    }
+
 
 }
