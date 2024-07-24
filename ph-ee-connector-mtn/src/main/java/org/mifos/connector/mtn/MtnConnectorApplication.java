b27933bda0b27b7b89c07ba947be9f9655bee028
Sourabh Kumar <148945054+shkr7@users.noreply.github.com>
2024-07-24 18:28:45 +0530

    Setup MTN Repo (#2)
    * Setting up MTN repo
    
    * setup MTN repo
    
    * Update circleCI config
    
    * Remove unnecessary dependency
    
    * Remove test module

---
+++ ph-ee-connector-mtn/src/main/java/org/mifos/connector/mtn/MtnConnectorApplication.java
@@ -0,0 +1,13 @@
+package org.mifos.connector.mtn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtnConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtnConnectorApplication.class, args);
+	}
+
+}
